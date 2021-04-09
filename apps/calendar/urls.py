89db7380ae07a04171de69b3196c9430fac76872
Michael Romashov <mromashov@icloud.com>
2021-04-09 17:58:32 -0400

    Consolidated calendar events into one endpoint.

--- apps/calendar/urls.py
+++ apps/calendar/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('events/', views.EventCalendarView.as_view()),
-    path('events/<int:year>/<int:month>/', views.EventCalendarView.as_view()),
-    path('training/', views.TrainingCalendarView.as_view()),
-    path('training/<int:year>/<int:month>/', views.TrainingCalendarView.as_view()),
+    path('', views.CalendarView.as_view()),
+    path('<int:year>/<int:month>/', views.CalendarView.as_view()),
 ]
--- apps/calendar/views.py
+++ apps/calendar/views.py
@@ -4,35 +4,36 @@ from rest_framework.views import APIView
 
 from zhu_core.permissions import ReadOnly
 from .serializers import *
+from ..booking.models import Booking
+from ..booking.serializers import BookingSerializer
 from ..training.models import Status
 
 
-class EventCalendarView(APIView):
+class CalendarView(APIView):
     permission_classes = [ReadOnly]
 
     def get(self, request, year=datetime.now().year, month=datetime.now().month):
         """
-        Get all events for given month and year.
+        Get all calendar events for given month and year.
         Defaults to current month of current year.
+        {
+            'events': [],
+            'sessions': [],
+            'bookings': [],
+        }
         """
         events = Event.objects.filter(start__month=month, start__year=year)
+        sessions = TrainingSession.objects.filter(start__month=month, start__year=year).exclude(status=Status.CANCELLED)
+        bookings = Booking.objects.filter(start__month=month, start__year=year)
 
         if not (request.user.is_authenticated and request.user.is_staff):
             events = events.exclude(hidden=True)
 
-        serializer = EventCalendarSerializer(events, many=True)
-        return Response(serializer.data)
-
-
-class TrainingCalendarView(APIView):
-    permission_classes = [ReadOnly]
-
-    def get(self, request, year=datetime.now().year, month=datetime.now().month):
-        """
-        Get all training sessions for given month and year.
-        Defaults to current month of current year.
-        """
-        sessions = TrainingSession.objects.filter(start__month=month, start__year=year).exclude(status=Status.CANCELLED)
-
-        serializer = TrainingCalendarSerializer(sessions, many=True)
-        return Response(serializer.data)
+        event_serializer = EventCalendarSerializer(events, many=True)
+        session_serializer = TrainingCalendarSerializer(sessions, many=True)
+        booking_serializer = BookingSerializer(bookings, many=True)
+        return Response({
+            'events': event_serializer.data,
+            'sessions': session_serializer.data,
+            'bookings': booking_serializer.data,
+        })
