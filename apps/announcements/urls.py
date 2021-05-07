3aaf73dc99997fd5b656bb289527021bb59b9dcf
Michael Romashov <mromashov@icloud.com>
2021-05-07 15:50:26 -0400

    Added DELETE to announcement methods.

--- apps/announcements/urls.py
+++ apps/announcements/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.AnnouncementListView.as_view()),
+    path('<int:announcement_id>/', views.AnnouncementInstanceView.as_view()),
     path('recent/', views.RecentAnnouncementListView.as_view()),
 ]
--- apps/announcements/views.py
+++ apps/announcements/views.py
@@ -1,5 +1,6 @@
 import bleach
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -30,6 +31,18 @@ class AnnouncementListView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AnnouncementInstanceView(APIView):
+    permission_classes = [IsStaff]
+
+    def delete(self, request, announcement_id):
+        """
+        Delete announcement
+        """
+        announcement = get_object_or_404(Announcement, id=announcement_id)
+        announcement.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class RecentAnnouncementListView(APIView):
