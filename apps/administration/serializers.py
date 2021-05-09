72b466e90e4a9e35d1bde116c79a5d30afda5ada
Michael Romashov <mromashov@icloud.com>
2021-05-09 18:17:38 -0400

    Representations for choice fields.

--- apps/administration/serializers.py
+++ apps/administration/serializers.py
@@ -1,4 +1,3 @@
-import json
 from auditlog.models import LogEntry
 from rest_framework import serializers
 
@@ -15,7 +14,22 @@ class LogEntrySerializer(serializers.ModelSerializer):
         fields = ['id', 'action', 'actor', 'changes', 'content_type', 'object_id', 'object_repr', 'timestamp']
 
     def get_changes(self, obj):
-        return json.loads(obj.changes)
+        """
+        The LogEntry attribute 'changes_display_dict' does not work for ManyToMany
+        related fields. This is a workaround that manually replaces choices with
+        their representations.
+        """
+        changes = obj.changes_dict
+        model = obj.content_type.model_class()
+
+        for field in changes:
+            field_class = model._meta.get_field(field)
+            if getattr(field_class, 'choices', None):
+                choices = {str(key): value for key, value in field_class.choices}
+                for i in range(2):
+                    changes[field][i] = choices.get(changes.get(field)[i])
+
+        return changes
 
     def get_content_type(self, obj):
         return obj.content_type.name
