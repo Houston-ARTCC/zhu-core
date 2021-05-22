0660fdc9c894cd48f3490b0253bfa0bae0f8d7b4
Michael Romashov <mromashov@icloud.com>
2021-05-21 20:22:19 -0400

    Updated new event webhook.

--- apps/events/signals.py
+++ apps/events/signals.py
@@ -7,33 +7,34 @@ from apps.events.models import Event
 
 
 @receiver(pre_save, sender=Event)
-def post_event_webhook(instance, **kwargs):
+def post_event_webhook(instance, created, **kwargs):
     """
     This signal posts an embedded message in the designated
     events Discord channel when an event is set to visible.
     """
-    old_instance = Event.objects.filter(id=instance.id)
-
-    if not old_instance.exists():
+    if instance.hidden:
         return
 
-    if old_instance.first().hidden and not instance.hidden:
-        url = f'https://www.zhuartcc.org/events/{instance.id}'
-        webhook = DiscordWebhook(url=os.getenv('EVENTS_WEBHOOK_URL'))
-        embed = DiscordEmbed(
-            title=f':calendar: {instance.name}',
-            description=instance.description + f'\n**[Sign up for the event here!]({url})**',
-            color='109cf1',
-        )
-        embed.add_embed_field(
-            name='Start & End',
-            value=f'{instance.start.strftime("%b %d, %Y @ %H%Mz")} - {instance.end.strftime("%b %d, %Y @ %H%Mz")}',
-            inline=False,
-        )
-        embed.add_embed_field(
-            name='Presented by',
-            value=instance.host,
-        )
-        embed.set_image(url=instance.banner)
-        webhook.add_embed(embed)
-        webhook.execute()
+    old_instance = Event.objects.filter(id=instance.id)
+    if old_instance.exists() and not old_instance.first().hidden:
+        return
+
+    url = f'https://www.zhuartcc.org/events/{instance.id}'
+    webhook = DiscordWebhook(url=os.getenv('EVENTS_WEBHOOK_URL'))
+    embed = DiscordEmbed(
+        title=f':calendar: {instance.name}',
+        description=instance.description + f'\n**[Sign up for the event here!]({url})**',
+        color='109cf1',
+    )
+    embed.add_embed_field(
+        name='Start & End',
+        value=f'{instance.start.strftime("%b %d, %Y @ %H%Mz")} - {instance.end.strftime("%b %d, %Y @ %H%Mz")}',
+        inline=False,
+    )
+    embed.add_embed_field(
+        name='Presented by',
+        value=instance.host,
+    )
+    embed.set_image(url=instance.banner)
+    webhook.add_embed(embed)
+    webhook.execute()
