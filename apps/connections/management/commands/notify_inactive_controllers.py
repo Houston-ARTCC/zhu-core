1dcabbff30c51961cbd9eaea7c4ab6e18e49bc09
Michael Romashov <mromashov@icloud.com>
2021-09-12 16:02:31 -0400

    Crap...

--- apps/connections/management/commands/notify_inactive_controllers.py
+++ apps/connections/management/commands/notify_inactive_controllers.py
@@ -1,39 +1,34 @@
 import os
-import pytz
 from datetime import datetime
 from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
 
-from zhu_core.utils import get_vatsim_data
-from apps.connections.models import OnlineController
-from apps.users.models import User
+from apps.connections.statistics import get_user_hours
+from apps.mailer.models import Email
+from apps.training.models import Status
 
 
 class Command(BaseCommand):
-    help = 'Pulls all connections from the VATSIM data API.'
+    help = 'Sends notification to controllers who have not fulfilled their activity requirements for the month.'
 
     def handle(self, *args, **options):
-        position_prefixes = os.getenv('POSITION_PREFIXES').split(',')
-        connections = {connection.get('callsign'): connection for connection in get_vatsim_data().get('controllers')}
+        if os.getenv('DEV_ENV') == 'True':
+            return
 
-        # Updates all controllers currently online.
-        for online in OnlineController.objects.all():
-            if online.callsign in connections and connections.get(online.callsign).get('cid') == online.user.cid:
-                online.save()
-            else:
-                online.convert_to_session()
-                online.delete()
+        hours = get_user_hours()
+        month_name = datetime.now().strftime('%B')
 
-        # Checks for new connections.
-        for callsign, connection in connections.items():
-            user = User.objects.filter(cid=connection.get('cid'))
-            if user.exists() and not OnlineController.objects.filter(callsign=callsign, user=user.first()).exists():
-                if callsign.split('_')[0] in position_prefixes and callsign.split('_')[-1] not in ['SUP', 'OBS']:
-                    OnlineController(
-                        user=user.first(),
-                        callsign=callsign,
-                        online_since=pytz.utc.localize(
-                            datetime.strptime(connection.get('logon_time')[:-2], '%Y-%m-%dT%H:%M:%S.%f')
-                        )
-                    ).save()
+        for user in hours.filter(status=Status.ACTIVE, roles__short__in=['HC', 'VC']):
+            if user.curr_hours < user.activity_requirement:
+                context = {
+                    'user': user,
+                    'month': month_name,
+                }
+                Email(
+                    subject='Controller Activity Reminder',
+                    html_body=render_to_string('activity_reminder.html', context=context),
+                    text_body=render_to_string('activity_reminder.txt', context=context),
+                    to_email=user.email,
+                ).save()
 
-        print(f'{datetime.now()} :: notify_inactive_controllers :: SUCCESS')
+        print(f'{datetime.now()} :: update_online_controllers :: SUCCESS')
--- apps/connections/management/commands/update_online_controllers.py
+++ apps/connections/management/commands/update_online_controllers.py
@@ -1,34 +1,39 @@
 import os
+import pytz
 from datetime import datetime
 from django.core.management.base import BaseCommand
-from django.template.loader import render_to_string
 
-from apps.connections.statistics import get_user_hours
-from apps.mailer.models import Email
-from apps.training.models import Status
+from zhu_core.utils import get_vatsim_data
+from apps.connections.models import OnlineController
+from apps.users.models import User
 
 
 class Command(BaseCommand):
-    help = 'Sends notification to controllers who have not fulfilled their activity requirements for the month.'
+    help = 'Pulls all connections from the VATSIM data API.'
 
     def handle(self, *args, **options):
-        if os.getenv('DEV_ENV') == 'True':
-            return
+        position_prefixes = os.getenv('POSITION_PREFIXES').split(',')
+        connections = {connection.get('callsign'): connection for connection in get_vatsim_data().get('controllers')}
 
-        hours = get_user_hours()
-        month_name = datetime.now().strftime('%B')
+        # Updates all controllers currently online.
+        for online in OnlineController.objects.all():
+            if online.callsign in connections and connections.get(online.callsign).get('cid') == online.user.cid:
+                online.save()
+            else:
+                online.convert_to_session()
+                online.delete()
 
-        for user in hours.filter(status=Status.ACTIVE, roles__short__in=['HC', 'VC']):
-            if user.curr_hours < user.activity_requirement:
-                context = {
-                    'user': user,
-                    'month': month_name,
-                }
-                Email(
-                    subject='Controller Activity Reminder',
-                    html_body=render_to_string('activity_reminder.html', context=context),
-                    text_body=render_to_string('activity_reminder.txt', context=context),
-                    to_email=user.email,
-                ).save()
+        # Checks for new connections.
+        for callsign, connection in connections.items():
+            user = User.objects.filter(cid=connection.get('cid'))
+            if user.exists() and not OnlineController.objects.filter(callsign=callsign, user=user.first()).exists():
+                if callsign.split('_')[0] in position_prefixes and callsign.split('_')[-1] not in ['SUP', 'OBS']:
+                    OnlineController(
+                        user=user.first(),
+                        callsign=callsign,
+                        online_since=pytz.utc.localize(
+                            datetime.strptime(connection.get('logon_time')[:-2], '%Y-%m-%dT%H:%M:%S.%f')
+                        )
+                    ).save()
 
-        print(f'{datetime.now()} :: update_online_controllers :: SUCCESS')
+        print(f'{datetime.now()} :: notify_inactive_controllers :: SUCCESS')
