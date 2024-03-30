96e473bc1a861ea6674b075641cd6098ecf4d3cf
Michael Romashov <mromashov@icloud.com>
2024-03-29 22:42:26 -0400

    fix: process cors middleware first

--- zhu_core/settings.py
+++ zhu_core/settings.py
@@ -114,6 +114,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -122,7 +123,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "zhu_core.middleware.JWTAuthenticationMiddleware",
     "auditlog.middleware.AuditlogMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
